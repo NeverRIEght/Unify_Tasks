56b7cd9061e328926f7c11a9d6716efcbb63ff0c
NeverRIEght <LFKomarovME@gmail.com>
2022-05-16 09:51:09 +0300

    Add text editing features

--- Unify_Tasks/DialogWindows/NoteEditor.xaml.cs
+++ Unify_Tasks/DialogWindows/NoteEditor.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -39,6 +40,7 @@ namespace Unify_Tasks.DialogWindows
             win.MaxWidth = 800;
             win.MaxHeight = 450;
 
+            //Loading prev file
             using (var context = new Unify_TasksEntities())
             {
                 var thisNote = context.Tasks.Where(n => n.TaskID == w1.currTask).FirstOrDefault();
@@ -55,9 +57,71 @@ namespace Unify_Tasks.DialogWindows
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ChangeSelectedText(DependencyProperty d, object value)
         {
+            var selection = NoteText.Selection;
+            if (!selection.IsEmpty)
+                selection.ApplyPropertyValue(d, value);
+        }
 
+        private void FontColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NoteText != null)
+            {
+                string selectedItem = FontColor.SelectedItem.ToString();
+                ChangeSelectedText(TextElement.ForegroundProperty, Regex.Replace(selectedItem.Trim(), @".*:\s+", ""));
+            }
+        }
+
+        private void FontStyle_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedItem = FontStyle.SelectedItem.ToString().Trim();
+
+            if (selectedItem == "System.Windows.Controls.ComboBoxItem: Main font")
+            {
+                ChangeSelectedText(TextElement.FontSizeProperty, Regex.Replace("14", @".*:\s+", ""));
+            }
+            if (selectedItem == "System.Windows.Controls.ComboBoxItem: Header 1")
+            {
+                ChangeSelectedText(TextElement.FontSizeProperty, Regex.Replace("32", @".*:\s+", ""));
+            }
+            if (selectedItem == "System.Windows.Controls.ComboBoxItem: Header 2")
+            {
+                ChangeSelectedText(TextElement.FontSizeProperty, Regex.Replace("24", @".*:\s+", ""));
+            }
+            if (selectedItem == "System.Windows.Controls.ComboBoxItem: Header 3")
+            {
+                ChangeSelectedText(TextElement.FontSizeProperty, Regex.Replace("18", @".*:\s+", ""));
+            }
+        }
+
+        private void BoldButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeSelectedText(TextElement.FontWeightProperty, "Bold");
+        }
+        private void BoldButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ChangeSelectedText(TextElement.FontWeightProperty, "Regular");
+        }
+        private void ItalicButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeSelectedText(TextElement.FontStyleProperty, "Italic");
+        }
+        private void ItalicButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ChangeSelectedText(TextElement.FontStyleProperty, "Normal");
+        }
+        private void UnderlineButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeSelectedText(TextBox.TextDecorationsProperty, "Underline");
+        }
+        private void UnderlineButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ChangeSelectedText(TextBox.TextDecorationsProperty, "None");
+        }
+
+        private void Save_MouseUp(object sender, MouseButtonEventArgs e)
+        {
             using (var context = new Unify_TasksEntities())
             {
                 var thisNote = context.Tasks.Where(n => n.TaskID == w1.currTask).FirstOrDefault();
